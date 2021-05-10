43fb57a7f04c61613554202539f84195b1116a0f
Isabelle Schmidt <isabelle.schmidt@student.hs-rm.de>
2021-05-10 22:16:20 +0200

    Blatt 1 weitergearbeitet noch ncith ganz fertig

--- src/main/java/de/hsrm/mi/web/projekt/sichtung/Sichtung.java
+++ src/main/java/de/hsrm/mi/web/projekt/sichtung/Sichtung.java
@@ -2,14 +2,22 @@ package de.hsrm.mi.web.projekt.sichtung;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class Sichtung {
     
     private String name;
     private String ort;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate datum;
     private String beschreibung;
     
     public Sichtung(String name, String ort, LocalDate datum, String beschreibung) {
+        this.name = name;
+        this.ort = ort;
+        this.beschreibung = beschreibung;
+        this.datum = datum;
     }
 
     public Sichtung(){
@@ -42,6 +50,5 @@ public class Sichtung {
     public String toString() {
         return "Sichtung [beschreibung=" + beschreibung + ", datum=" + datum + ", name=" + name + ", ort=" + ort + "]";
     }
-    
     
 }
--- src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
+++ src/main/java/de/hsrm/mi/web/projekt/sichtung/SichtungController.java
@@ -9,29 +9,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes(names = {"meinesichtungen"})
-
 public class SichtungController {
     
     Logger logger = LoggerFactory.getLogger(SichtungController.class);
     
-    ArrayList<Sichtung> meinesichtungen = new ArrayList<Sichtung>(); 
-
     @ModelAttribute("meinesichtungen")
-    public void initListe(Model m){
-
+    public void initListe(Model m){ 
+        ArrayList<Sichtung> meinesichtungen = new ArrayList<Sichtung>(); 
+        
         final Sichtung s1 = new Sichtung();
         s1.setName("Hans");
         s1.setOrt("Benscrime");
         s1.setDatum(LocalDate.parse("2021-05-05"));
         s1.setBeschreibung("17 Sichtung in einer Bluuuume");
         logger.info("liste init {}",s1);
-
+        
         final Sichtung s2 = new Sichtung();
         s2.setName("Gerturde");
         s2.setOrt("Heppenheim Altstadt");
@@ -39,25 +37,42 @@ public class SichtungController {
         s2.setBeschreibung("17 Sichtung bei meiner Sushi Bestellung");
         logger.info("liste init {}",s2);
 
+        m.addAttribute("meinesichtunge", meinesichtungen);
         meinesichtungen.add(s1);
-        m.addAttribute("meinesichtungen", meinesichtungen);
+        meinesichtungen.add(s2);
     }
-
+    
     @GetMapping("/sichtung/meine")
     public String merkesichtungen(Model m){
-        m.addAttribute("meinesichtungen", meinesichtungen);
         return "sichtung/meine/liste";
     }
 
-    @PostMapping("/sichtung/meine")
-    public String postForm(@ModelAttribute("angebote") ArrayList<Sichtung> liste, Model m){
-        // if(bind.hasErrors()){
-        //     logger.info("Formular neu Ausfuellen wenn kek was falsch macht");
-        //     m.addAttribute("angebotform", braten);
-        //     return "angebote/bearbeiten";
-        // }
-        return "angebote/liste";
+    @GetMapping("/sichtung/meine/neu")
+    public String getform(Model m){
+        m.addAttribute("meinesichtungform", new Sichtung());
+        return "sichtung/meine/bearbeiten";
     }
 
+    @GetMapping("/sichtung/meine/{i}/del")
+    public String del(@PathVariable int i,  @ModelAttribute("meinesichtungen") ArrayList<Sichtung> liste ){
+        liste.remove(i);
+        return "redirect:/sichtung/meine";
+    }
+
+    @GetMapping("/sichtung/meine/{i}")
+    public String edit(@PathVariable("i") int i, @ModelAttribute("meinesichtungform") Sichtung sichtung, @ModelAttribute("meinesichtungen") ArrayList<Sichtung> liste){
+        sichtung.setName(liste.get(i).getName());
+        sichtung.setBeschreibung(liste.get(i).getBeschreibung());
+        liste.remove(i);
+        return "/sichtung/meine/bearbeiten";
+    }
+
+    @PostMapping("/sichtung/meine/neu")
+    public String postForm(@ModelAttribute("meinesichtungform") Sichtung sichtung,
+                           @ModelAttribute("meinesichtungen") ArrayList<Sichtung> liste, Model m){  
+        logger.info("Olli ist toll");
+        liste.add(sichtung);
+        return "redirect:/sichtung/meine";
+    }
 
 }
