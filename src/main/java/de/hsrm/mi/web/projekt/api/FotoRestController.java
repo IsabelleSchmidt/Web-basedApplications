c8c59c8bddf0def4008c6e39b3446b0f7f308566
Isabelle Schmidt <isabelle.schmidt@student.hs-rm.de>
2021-06-01 23:41:43 +0200

    RestController fertig

--- src/main/java/de/hsrm/mi/web/projekt/api/FotoRestController.java
+++ src/main/java/de/hsrm/mi/web/projekt/api/FotoRestController.java
@@ -1,16 +1,14 @@
 package de.hsrm.mi.web.projekt.api;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.hsrm.mi.web.projekt.foto.Foto;
@@ -18,35 +16,37 @@ import de.hsrm.mi.web.projekt.foto.FotoService;
 import de.hsrm.mi.web.projekt.foto.Kommentar;
 
 @RestController
+@RequestMapping("/api")
 public class FotoRestController {
 
     Logger logger = LoggerFactory.getLogger(FotoRestController.class);
 
     @Autowired
-    private FotoService fotoService;
+    FotoService fotoService;
 
-    public void setFotoservice(FotoService fotoService){
-        this.fotoService = fotoService;
+    // public void setFotoservice(FotoService fotoService){
+    //     this.fotoService = fotoService;
+    // }
+
+    @GetMapping("/foto")
+    public List<Foto> getApiFoto() {
+        return fotoService.alleFotosNachZeitstempelSortiert();
     }
 
-    @GetMapping("/api/foto")
-    public List<Foto> fotoList(@ModelAttribute("fotos") ArrayList<Foto> fotol, Model m){
-        m.addAttribute("fotos", fotoService.alleFotosNachZeitstempelSortiert());
-        return null;
-    }
-
-    @DeleteMapping("/api/foto/{id}")
+    @DeleteMapping("/foto/{id}")
     public void deleteFoto(@PathVariable("id") Long id){
-        fotoService.loescheFoto(id);
+        if(fotoService.fotoAbfragenNachId(id).isPresent()){
+            fotoService.loescheFoto(id);
+        }
     }
 
-    @GetMapping("/api/foto/{id}")
-    public List<Kommentar> listKommentare(){
-        return null;
+    @GetMapping("/foto/{id}/kommentar")
+    public List<Kommentar> getFotoKommentare(@PathVariable long id) {
+        return fotoService.alleKommentareFuerFoto(id);
     }
 
-    @DeleteMapping("/api/foto/{id}/kommentar/{kid}")
+    @DeleteMapping("/foto/{id}/kommentar/{kid}")
     public void deleteKommentar(@PathVariable("id") Long id, @PathVariable("kid") Long kid){
-
+        fotoService.fotoKommentarLoeschen(id, kid);
     }
 }
